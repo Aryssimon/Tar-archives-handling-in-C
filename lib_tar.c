2893b493c823d8c51fa837b77ee5ef111dcf6c3f
Simon Arys <simon.arys@student.uclouvain.be>
2021-12-14 18:15:41 +0100

    Implements the list() function but needs to be tested on Inginious

--- lib_tar.c
+++ lib_tar.c
@@ -9,6 +9,41 @@ int at_end_of_archive(int tar_fd) {
     return 1;
 }
 
+int get_real_path(int tar_fd, char *path, char *real_path) {
+    lseek(tar_fd, 0, SEEK_SET);
+    unsigned int atSize = 0;
+    char linkname[100];
+    while(at_end_of_archive(tar_fd) == 0){
+        char name[100];
+        lseek(tar_fd,atSize,SEEK_SET);
+        if (read(tar_fd, &name, 100) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        if (strcmp(name, path) == 0) {
+            lseek(tar_fd, 56, SEEK_CUR);
+            char typeflag[1];
+            if (read(tar_fd, &typeflag, 1) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+            if (typeflag[0] == SYMTYPE) {
+                if (read(tar_fd, linkname, 100) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+                if (is_symlink(tar_fd, linkname)) {
+                    return get_real_path(tar_fd, linkname, real_path);
+                } else {
+                    strcpy(real_path, linkname);
+                    return 1;
+                }
+            } else {
+                return 0;
+            }
+        }
+
+        char size[12];
+        lseek(tar_fd, atSize+124, SEEK_SET);
+        if (read(tar_fd, &size, 12) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        unsigned int fileSize = 512+(512*((TAR_INT(size)/512)+(TAR_INT(size)%512 != 0)));
+        atSize += fileSize;
+        lseek(tar_fd, atSize, SEEK_SET);
+    }
+    return -1;
+}
+
 /**
  * Checks whether the archive is valid.
  *
@@ -27,6 +62,7 @@ int at_end_of_archive(int tar_fd) {
 int check_archive(int tar_fd) {
     lseek(tar_fd,0,SEEK_SET);
     unsigned int atSize = 0;
+    int headers_count = 0;
     while (at_end_of_archive(tar_fd) == 0) {
         char name[100];
         lseek(tar_fd,atSize,SEEK_SET);
@@ -60,7 +96,11 @@ int check_archive(int tar_fd) {
         lseek(tar_fd, atSize+148, SEEK_SET);
         char chksum[8];
         if (read(tar_fd, &chksum, 8) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
-        if (TAR_INT(chksum) != chksumCounter) return -3;
+        if (TAR_INT(chksum) != chksumCounter) {
+            return -3;
+        } else {
+            headers_count++;
+        }
 
         //Go to next header
         char size[12];
@@ -71,7 +111,7 @@ int check_archive(int tar_fd) {
         lseek(tar_fd, atSize, SEEK_SET);
     }
 
-    return 0;
+    return headers_count;
 }
 
 /**
@@ -112,7 +152,26 @@ int exists(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int is_dir(int tar_fd, char *path) {
-    exists(tar_fd, path);
+    lseek(tar_fd,0,SEEK_SET);
+    unsigned int atSize = 0;
+    while(at_end_of_archive(tar_fd) == 0){
+        char name[100];
+        lseek(tar_fd,atSize,SEEK_SET);
+        if (read(tar_fd, &name, 100) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        if (strcmp(name, path) == 0) {
+            lseek(tar_fd, 56, SEEK_CUR);
+            char typeflag[1];
+            if (read(tar_fd, &typeflag, 1) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+            return typeflag[0] == DIRTYPE;
+        }
+
+        char size[12];
+        lseek(tar_fd, atSize+124, SEEK_SET);
+        if (read(tar_fd, &size, 12) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        unsigned int fileSize = 512+(512*((TAR_INT(size)/512)+(TAR_INT(size)%512 != 0)));
+        atSize += fileSize;
+        lseek(tar_fd, atSize, SEEK_SET);
+    }
     return 0;
 }
 
@@ -126,6 +185,26 @@ int is_dir(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int is_file(int tar_fd, char *path) {
+    lseek(tar_fd,0,SEEK_SET);
+    unsigned int atSize = 0;
+    while(at_end_of_archive(tar_fd) == 0){
+        char name[100];
+        lseek(tar_fd,atSize,SEEK_SET);
+        if (read(tar_fd, &name, 100) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        if (strcmp(name, path) == 0) {
+            lseek(tar_fd, 56, SEEK_CUR);
+            char typeflag[1];
+            if (read(tar_fd, &typeflag, 1) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+            return typeflag[0] == REGTYPE || typeflag[0] == AREGTYPE;
+        }
+
+        char size[12];
+        lseek(tar_fd, atSize+124, SEEK_SET);
+        if (read(tar_fd, &size, 12) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        unsigned int fileSize = 512+(512*((TAR_INT(size)/512)+(TAR_INT(size)%512 != 0)));
+        atSize += fileSize;
+        lseek(tar_fd, atSize, SEEK_SET);
+    }
     return 0;
 }
 
@@ -138,6 +217,26 @@ int is_file(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int is_symlink(int tar_fd, char *path) {
+    lseek(tar_fd,0,SEEK_SET);
+    unsigned int atSize = 0;
+    while(at_end_of_archive(tar_fd) == 0){
+        char name[100];
+        lseek(tar_fd,atSize,SEEK_SET);
+        if (read(tar_fd, &name, 100) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        if (strcmp(name, path) == 0) {
+            lseek(tar_fd, 56, SEEK_CUR);
+            char typeflag[1];
+            if (read(tar_fd, &typeflag, 1) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+            return typeflag[0] == SYMTYPE;
+        }
+
+        char size[12];
+        lseek(tar_fd, atSize+124, SEEK_SET);
+        if (read(tar_fd, &size, 12) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        unsigned int fileSize = 512+(512*((TAR_INT(size)/512)+(TAR_INT(size)%512 != 0)));
+        atSize += fileSize;
+        lseek(tar_fd, atSize, SEEK_SET);
+    }
     return 0;
 }
 
@@ -165,7 +264,46 @@ int is_symlink(int tar_fd, char *path) {
  *         any other value otherwise.
  */
 int list(int tar_fd, char *path, char **entries, size_t *no_entries) {
-    return 0;
+
+    char real_path[100];
+
+    if (is_symlink(tar_fd, path) == 1) {
+        get_real_path(tar_fd, path, real_path);
+    } else {
+        strcpy(real_path, path);
+    }
+    printf("path= %s\n", path);
+    printf("real_path= %s\n", real_path);
+
+    if (is_dir(tar_fd, real_path) == 0) return 0;
+
+    lseek(tar_fd,0,SEEK_SET);
+    int atSize = 0;
+    unsigned int entries_idx = 0;
+
+    while(at_end_of_archive(tar_fd) == 0){
+        char name[100];
+        lseek(tar_fd,atSize,SEEK_SET);
+        if (read(tar_fd, &name, 100) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        if (strncmp(name, real_path, strlen(real_path)) == 0 && strlen(name) > strlen(real_path)) {
+            int i;
+            for (i = strlen(real_path); name[i] != '/' && name[i] != '\0'; i++);
+            if ((name[i] == '/' && name[i+1] == '\0') || name[i] == '\0') {
+                if (entries_idx < *no_entries) {
+                    strcpy(entries[entries_idx], name);
+                    entries_idx++;
+                }
+            }
+        }
+        char size[12];
+        lseek(tar_fd, atSize+124, SEEK_SET);
+        if (read(tar_fd, &size, 12) == -1) fprintf(stderr, "read failed, errno = %d\n", errno);
+        unsigned int fileSize = 512+(512*((TAR_INT(size)/512)+(TAR_INT(size)%512 != 0)));
+        atSize += fileSize;
+        lseek(tar_fd, atSize, SEEK_SET);
+    }
+    *no_entries = entries_idx;
+    return 1;
 }
 
 /**
@@ -187,5 +325,16 @@ int list(int tar_fd, char *path, char **entries, size_t *no_entries) {
  *
  */
 ssize_t read_file(int tar_fd, char *path, size_t offset, uint8_t *dest, size_t *len) {
+
+    char real_path[100];
+
+    if (is_symlink(tar_fd, path) == 1) {
+        get_real_path(tar_fd, path, real_path);
+    } else {
+        strcpy(real_path, path);
+    }
+
+    if (is_file(tar_fd, path) == 0) return -1;
+    
     return 0;
 }
--- tests.c
+++ tests.c
@@ -41,20 +41,100 @@ int main(int argc, char **argv) {
 
     // Tests for exists()
     ret = exists(fd,"hints.txt");
-    printf("check hints.txt %d\n", ret);
-    ret = exists(fd,"lib_tar.c");
-    printf("check lib_tar.c %d\n", ret);
+    printf("check hints.txt %d\n", ret); //1
+    ret = exists(fd,"README.md");
+    printf("check README.md %d\n", ret); //1
     ret = exists(fd,"myfolder/");
-    printf("check myfolder/ %d\n", ret);
+    printf("check myfolder/ %d\n", ret); //1
     ret = exists(fd,"myfolder/README.md");
-    printf("check myfolder/README.md %d\n", ret);
+    printf("check myfolder/README.md %d\n", ret); //1
     ret = exists(fd,"myfolder/tests.c");
-    printf("check myfolder/tests.c %d\n", ret);
+    printf("check myfolder/myfolderinfolder/mytextfile.txt %d\n", ret); //1
 
     ret = exists(fd,"nothing");
-    printf("check nothing %d\n", ret);
+    printf("check nothing %d\n", ret); //0
     ret = exists(fd,"falsetest.txt");
-    printf("check falsetest.txt %d\n", ret);
+    printf("check falsetest.txt %d\n", ret); //0
+
+    // Tests for is_dir()
+    ret = is_dir(fd,"hints.txt");
+    printf("check hints.txt %d\n", ret); //0
+    ret = is_dir(fd,"myfolder/");
+    printf("check myfolder/ %d\n", ret); //1
+    ret = is_dir(fd,"myfolder");
+    printf("check myfolder %d\n", ret); //0
+
+    // Tests for is_file()
+    ret = is_file(fd,"hints.txt");
+    printf("check hints.txt %d\n", ret); //1
+    ret = is_file(fd,"myfolder/");
+    printf("check myfolder/ %d\n", ret); //0
+    ret = is_file(fd,"myfolder");
+    printf("check myfolder %d\n", ret); //0
+
+    // Tests for is_symlink()
+    ret = is_symlink(fd,"hints.txt");
+    printf("check hints.txt %d\n", ret); //0
+    ret = is_symlink(fd,"myfolder/");
+    printf("check myfolder/ %d\n", ret); //0
+    ret = is_symlink(fd,"myfolder");
+    printf("check myfolder %d\n", ret); //0
+    ret = is_symlink(fd,"myfolder/mylink.txt");
+    printf("check myfolder/mylink.txt %d\n", ret); //0
+    ret = is_symlink(fd,"myfolder/mysymlink.txt");
+    printf("check myfolder/mysymlink.txt %d\n", ret); //1
+
+    // Tests for list()
+    char *arrayofarrays[10];
+    char array0[100];
+    char array1[100];
+    char array2[100];
+    char array3[100];
+    char array4[100];
+    char array5[100];
+    char array6[100];
+    char array7[100];
+    char array8[100];
+    char array9[100];
+    arrayofarrays[0] = array0;
+    arrayofarrays[1] = array1;
+    arrayofarrays[2] = array2;
+    arrayofarrays[3] = array3;
+    arrayofarrays[4] = array4;
+    arrayofarrays[5] = array5;
+    arrayofarrays[6] = array6;
+    arrayofarrays[7] = array7;
+    arrayofarrays[8] = array8;
+    arrayofarrays[9] = array9;
+    size_t* no_entries = (size_t*)malloc(sizeof(size_t));
+    *no_entries = 10;
+    ret = list(fd,"myfolder/", arrayofarrays, no_entries);
+    printf(" -- check myfolder/ %d\n", ret); //1
+    for (int i = 0; i < *no_entries; i++) printf("entry %d : %s\n",i,arrayofarrays[i]);
+    free(no_entries);
+
+    char ** entries2 = (char**) malloc(sizeof(char*)*20);
+    for (int i = 0; i < 20; i++) {
+        entries2[i] = (char*) malloc(sizeof(char)*100);
+    }
+
+    size_t* no_entries2 = (size_t*)malloc(sizeof(size_t));
+    *no_entries2 = 20;
+    ret = list(fd,"myfolder/myfolderinfolder/mysecondfolder/symfromdepth", entries2, no_entries2);
+    printf(" -- check myfolder/myfolderinfolder/mysecondfolder/symfromdepth %d\n", ret); //1
+    for (int i = 0; i < *no_entries2; i++) printf("entry %d : %s\n",i,entries2[i]);
+
+
+    char ** entries3 = (char**) malloc(sizeof(char*)*20);
+    for (int i = 0; i < 20; i++) {
+        entries3[i] = (char*) malloc(sizeof(char)*100);
+    }
+
+    size_t* no_entries3 = (size_t*)malloc(sizeof(size_t));
+    *no_entries3 = 20;
+    ret = list(fd,"myfolder/myfolderinfolder/mysecondfolder/symfromdepth2", entries3, no_entries3);
+    printf(" -- check myfolder/myfolderinfolder/mysecondfolder/symfromdepth2 %d\n", ret); //1
+    for (int i = 0; i < *no_entries3; i++) printf("entry %d : %s\n",i,entries3[i]);
 
     return 0;
 }
